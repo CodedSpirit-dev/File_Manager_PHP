ce2d2d7f2618831591796453e027ffc802f3dae3
Victor Irad Verdeja Trujillo <VIradVT@gmail.com>
2024-10-19 00:20:36 -0600

    Correcion app

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC-6'),
+    'timezone' => env('APP_TIMEZONE', 'America/Mexico_City'),
 
     /*
     |--------------------------------------------------------------------------
