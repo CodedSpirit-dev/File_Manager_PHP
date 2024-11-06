340aed0e2798ad2a1843261bd59cfc4bb2cdeae0
Victor Irad Verdeja Trujillo <VIradVT@gmail.com>
2024-11-06 14:35:06 -0600

    Cambiando de proveedor de PDF utils

--- resources/js/Pages/FileSystem/Components/FileViewer.tsx
+++ resources/js/Pages/FileSystem/Components/FileViewer.tsx
@@ -6,10 +6,7 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    'pdfjs-dist/build/pdf.worker.min.mjs',
-    import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 interface FileViewerProps {
     fileUrl: string;
